b21ac3caef49cbb6351fbde06b537c477833e8a7
Seth B. Altshuler <sethb78@gmail.com>
2012-08-13 19:03:12 -0400

    Add SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,9 @@ SampleApp::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
-
+# Force all access to the app over SSL, use Strict-Transport-Security, 
+  # and use secure cookies.
+  config.force_ssl = true
   # Generate digests for assets URLs
   config.assets.digest = true
 
